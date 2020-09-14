e2c6419bc04855efb086ddc016841970bda66369
Muhammed khaled <muhammmed.k95@gmail.com>
2020-09-15 00:14:17 +0200

    catogries, new products  added

--- client/src/components/views/LandingPage/Sections/RadioBox.js
+++ client/src/components/views/LandingPage/Sections/RadioBox.js
@@ -1,36 +1,36 @@
-import React, { useState } from 'react'
-import { Collapse, Radio } from 'antd';
+import React, { useState } from "react";
+import { Collapse, Radio } from "antd";
 const { Panel } = Collapse;
 
-
 function RadioBox(props) {
+  const [Value, setValue] = useState("0");
 
-    const [Value, setValue] = useState('0')
+  const renderRadioBox = () =>
+    props.list &&
+    props.list.map((value) => (
+      <div style={{ margin: 1 }}>
+        <Radio key={value._id} value={`${value._id}`}>
+          {value.name}
+        </Radio>
+      </div>
+    ));
 
-    const renderRadioBox = () => (
-        props.list &&  props.list.map((value) => (
-            <Radio key={value._id} value={`${value._id}`}>{value.name}</Radio>
-        ))
-    )
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    props.handleFilters(event.target.value);
+  };
 
-    const handleChange = (event) => {
-        setValue(event.target.value)
-        props.handleFilters(event.target.value)
-    }
-
-    return (
-        <div>
-            <Collapse defaultActiveKey={['0']}>
-                <Panel header="price" key="1">
-                    <Radio.Group onChange={handleChange} value={Value}>
-
-                        {renderRadioBox()}
-
-                    </Radio.Group>
-                </Panel>
-            </Collapse>
-        </div>
-    )
+  return (
+    <div>
+      <Collapse defaultActiveKey={["0"]}>
+        <Panel header="price" key="1">
+          <Radio.Group onChange={handleChange} value={Value}>
+            {renderRadioBox()}
+          </Radio.Group>
+        </Panel>
+      </Collapse>
+    </div>
+  );
 }
 
-export default RadioBox
+export default RadioBox;
