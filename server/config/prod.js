845f37f5f8eeccc31a58c577ad75498c7becc206
Muhammed khaled <muhammmed.k95@gmail.com>
2020-09-14 22:53:22 +0200

    heroku-post-build added

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
+  //   mongoURI: process.env.MONGO_URI,
+  mongoURI:
+    "mongodb+srv://medo1995:medo1995@react-shop-app.uhvh6.mongodb.net/<dbname>?retryWrites=true&w=majority",
 };
